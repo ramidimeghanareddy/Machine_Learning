e43c3e8524542bc667f56fe307de4d9f8b315537
Meghana Ramidi <129314031+ramidimeghanareddy@users.noreply.github.com>
2023-08-14 21:18:26 -0500

    Add files via upload

---
+++ Project2/Q2/Q2_D.py
@@ -0,0 +1,89 @@
+def main():
+    print('START Q2_D\n')
+
+    import numpy as np
+
+    def mean_squared_error(y_true, y_pred):
+        return np.mean((y_true-y_pred)**2)
+    
+	# Data Pre-processing and fetching the data to clean it
+    def data_cleaning(line):
+        return line.replace('(', '').replace(')', '').replace(' ', '').strip().split(',')
+
+    def data_fetching(filename):
+        with open(filename, 'r') as f:
+            data_input = f.readlines()
+            cleaning_input = list(map(data_cleaning, data_input))
+            f.close()
+        return cleaning_input
+    
+	# Reading the file after cleaning
+    def readFile(dataset_path):
+        data_input = data_fetching(dataset_path)
+        np_input = np.array(data_input)
+        return np_input
+    
+    # Implementing a locally weighted linear regression function
+    def weighted_linear_regression(x0, X, Y, gamma = 0.204):
+        # add bias term
+        x0 = np.r_[1, x0]
+        X = np.c_[np.ones(len(X)), X]
+
+        # fit model: normal equations with kernel
+        xw = X.T * calculating_weights(x0, X, gamma = 0.204)
+        theta = np.linalg.pinv(xw @ X) @ xw @ Y
+        # "@" is used to
+        # predict value
+        return x0 @ theta
+
+    # Function for calculating weight
+    def calculating_weights(x0, X, gamma = 0.204):
+        return np.exp(np.sum((X - x0) ** 2, axis=1) / (-2 * (gamma **2) ))
+
+    data = './datasets/Q1_B_train.txt'
+    np_data = readFile(data)
+
+    # X and Y separation
+    X = []
+    for i in range(np_data.shape[0]):
+        X.append(np.array(np_data[i][:-1], dtype=np.float32))
+    X = np.array(X)
+
+    Y = []
+    for i in range(np_data.shape[0]):
+        Y.append([np_data[i][-1]])
+    Y = np.array(Y, dtype=np.float32)
+    
+    X = X[:20]
+    Y = Y[:20]
+
+    Y_pred = [weighted_linear_regression(x0, X, Y) for x0 in X]
+
+    import matplotlib.pyplot as plt
+    plt.scatter(X, Y_pred)
+    plt.title('Locally Weighted Linear Regression')
+    plt.show()
+    
+    data = './datasets/Q1_C_test.txt'
+    np_data = readFile(data)
+
+    # test_X and test_Y separation
+    test_X = []
+    for i in range(np_data.shape[0]):
+        test_X.append(np.array(np_data[i][:-1], dtype=np.float32))
+    test_X = np.concatenate( test_X, axis=0 )
+
+
+    test_Y = []
+    for i in range(np_data.shape[0]):
+        test_Y.append([np_data[i][-1]])
+    test_Y = np.array(test_Y, dtype=np.float32)
+
+    test_Y_pred = [weighted_linear_regression(x0, X, Y) for x0 in test_X]
+    test_Y_pred = np.concatenate(test_Y_pred, axis=0)
+
+    print(f'data size = {len(Y)}, MSE = {mean_squared_error(test_Y, test_Y_pred)}')
+    print('END Q2_D\n')
+
+if __name__ == "__main__":
+    main()  
---
+++ Project2/Q2/readme2.txt
@@ -0,0 +1,19 @@
+
+Student Name: Meghana Ramidi
+UTA Student ID: 1002036880
+Student NetID: mxr6880
+
+Q2
+
+a) Implemented a locally weighted linear regression learner to solve the best fit problem for 1-dimensional data.
+
+b) Applied locally weighted linear regression learner to the data set that was generated for Question 1b) and created a plot for the resulting function together with the data points
+
+c) Evaluated the locally weighted linear regression on the Test data from Question 1 c). Compared to the results in part 1c, we can see that there is an overall increase in the MSE values, so the earlier model is performing better.
+
+d) Repeated the experiment and evaluation of parts b) and c) using only the first 20 elements of the training data set part b) and the test set of part c). 
+Compared to the results in part 1d, we can see that there is an overall increase in the MSE values, and thus this model is underperforming. Also, the change in MSE values was not very high between part 1c and 1d, but here there is quite an increase in the MSE value after fitting the model on only the first 20 data points.
+
+e) As we can see from parts c and d, the mean squared error increases from 0.674 to 1.018 when we restrict the training dataset to the first 20 data points. 
+Also, from the plots we can see that the weighted linear regression curve was better fitting when we considered the entire training dataset, yet, it is quite random in nature. 
+So, we conclude that the data points weren't actually derived from a function that is consistent with the function format in Question 1.
